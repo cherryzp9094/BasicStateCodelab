f539041eacf9b86324ba8b393740dcf86aac7734
cherry <cherryzp9094@gmail.com>
2023-05-21 17:39:47 +0900

    feat: 구성 가능한 함수의 메모리

--- app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/codelabs/basicstatecodelab/WaterCounter.kt
@@ -5,6 +5,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -14,7 +19,7 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier.padding(16.dp)
     ) {
-        var count = 0
+        var count by remember { mutableStateOf(0) }
         Text(
             text = "You've had $count glasses.",
         )
